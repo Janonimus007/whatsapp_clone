abaf4dfc37992f44c04d59584c81b65e0aa403b5
Alejandro Vera <avera@webclass.com>
2024-11-12 19:29:36 -0300

    agregar data ficticia

--- src/Screens/Chats/ChatsScreen.tsx
+++ src/Screens/Chats/ChatsScreen.tsx
@@ -2,12 +2,12 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import ChatListItem from '../../Components/ChatListItem'
 import { styles } from './ChatScreen.styles'
-
+import ChatRooms from '../../data/ChatRooms'
 
 export default function ChatsScreen() {
   return (
     <View style={styles.container}>
-      <ChatListItem chatRoom={{lastMessage:{content:"Hello There"}}}/>
+      <ChatListItem chatRoom={ChatRooms[0]}/>
     </View>
   )
 }
